ad0049cc5a8ef5a7b52efa011a5a08406e6df72f
Khokhlov Alexander <Alexhoxlov@ya.ru>
2019-03-30 13:04:45 +0300

    дз №2: адаптив

---
+++ src/scripts/parallax.js
@@ -0,0 +1,20 @@
+import { arrayExpression } from "babel-types";
+
+const parallax = document.querySelector(".parallax");
+const layers = parallax.children;
+
+console.log('layers');
+
+function moveLayers(wScroll) {
+    Array.from(layers).forEach(layer => {
+        const divider = layer.dataset.speed;
+        const strafe = wScroll * divider;
+
+        layer.style.transform = `translateY(-${strafe}%)`;
+    });
+};
+
+window.addEventListener("scroll", e=> {
+    const wScroll = window.pageYOffset;
+    moveLayers(wScroll);
+})
