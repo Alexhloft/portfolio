73f35dd28fc78f8779f148433ee4c043405e88c4
Khokhlov Alexander <Alexhoxlov@ya.ru>
2019-04-03 23:05:16 +0300

    адаптив, параллакс, вью

---
+++ src/scripts/reviews.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Flickity from 'vue-flickity';
+
+new Vue({
+
+    components: {
+        Flickity
+    },
+
+    data () {
+        return {
+            flickityOptions: {
+                prevNextButtons: false,
+                pageDots: false,
+                groupCells: 2
+
+            }
+        }
+    },
+
+    methods: {
+        next() {
+            this.$refs.flickity.next();
+        },
+
+        previous() {
+            this.$refs.flickity.previous();
+        }
+    }
+});
