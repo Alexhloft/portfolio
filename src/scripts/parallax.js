4de6af570bc3dbed14ed22d1b84ca601397d97d8
Khokhlov Alexander <Alexhoxlov@ya.ru>
2019-04-09 16:46:07 +0300

    works slider

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -3,8 +3,6 @@ import { arrayExpression } from "babel-types";
 const parallax = document.querySelector(".parallax__inner");
 const layers = parallax.children;
 
-console.log('layers');
-
 function moveLayers(wScroll) {
     Array.from(layers).forEach(layer => {
         const divider = layer.dataset.speed;
